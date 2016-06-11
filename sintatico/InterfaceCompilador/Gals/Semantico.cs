a72e7c9edadbee6da5e856928b9f36670255236a
ivanmanoel96 <ivanmanoel96@hotmail.com>
2016-06-11 18:34:49 -0300

    Update Semantico.cs

--- sintatico/InterfaceCompilador/Gals/Semantico.cs
+++ sintatico/InterfaceCompilador/Gals/Semantico.cs
@@ -1,11 +1,85 @@
 using System;
+using System.Collections;
+using System.Text;
+
 namespace InterfaceCompilador.Gals
 {
     public class Semantico : Constants
     {
+        StringBuilder codigo = new StringBuilder();
+        Stack pilha = new Stack();
+        string operadorRelacional;
+
         public void executeAction(int action, Token token)
         {
-            Console.WriteLine("Semantico não implementado");
-        }	
+            int acaoSemantica = action - FIRST_SEMANTIC_ACTION;
+
+            switch (acaoSemantica)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    acao10(token);
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    acao12();
+                    break;
+                case 13:
+                    acao13();
+                    break;
+                case 14:
+                    break;
+                case 15:
+                    break;
+                case 16:
+                    break;
+                case 17:
+                    break;
+                case 18:
+                    break;
+                case 19:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void acao10(Token token)
+        {
+            operadorRelacional = token.lexema;
+        }
+
+        private void acao12()
+        {
+            codigo.AppendLine(".assembly extern mscorlib {}");
+            codigo.AppendLine(".assembly codigo_objeto{}");
+            codigo.AppendLine(".module codigo_objeto.exe");
+            codigo.AppendLine("");
+            codigo.AppendLine(".method static public void _principal(){");
+            codigo.AppendLine(".entrypoint )");
+        }
+
+        private void acao13()
+        {
+            codigo.AppendLine("ret } }");
+        }
     }
 }
